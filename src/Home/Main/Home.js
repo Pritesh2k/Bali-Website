19678076559fb2bb6645cba7f7bb64cf4718304d
Pritesh2k <priteshparekh237@gmail.com>
2024-12-07 15:38:36 +0000

    Homepage v1.1

--- src/Home/Main/Home.js
+++ src/Home/Main/Home.js
@@ -1,17 +1,71 @@
-import React from 'react'
-import '../CSS/home.css'
+import React from 'react';
+import '../CSS/home.css';
+
+function openLink() {
+    window.open('https://google.com', '_blank');
+  }
 
 function Home() {
   return (
     <div className='home-wrapper'>
+        <div className='triangle-wrapper'>
+            {/* Custom Button */}
+            <button className='custom-button' onClick={()=>{openLink()}}>
+            <div className='line one'>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+            </div>
+            <div className='line two'>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+            </div>
+            <div className='line three'>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+            </div>
+            <div className='line four'>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+            </div>
+            <div className='line five'>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+            </div>
+            <div className='line six'>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+            </div>
+            <div className='line seven'>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+                <div className='round'></div>
+            </div>
+            </button>
+            <div class="hover-text"><i>Back to Portfolio 1.0?</i></div>
+        </div>
+
         <div className='title'>
             <h1>Portfolio</h1>
         </div>
+        <div className='main-line'></div>
         <div className='version'>
             <h1>2.0</h1>
         </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

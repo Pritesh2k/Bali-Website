120729b2aeffcada21d2320947c6cd8e86ae43cd
Pritesh2k <priteshparekh237@gmail.com>
2024-12-07 14:33:04 +0000

    HomePage v1

---
+++ src/Home/Main/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../CSS/home.css'
+
+function Home() {
+  return (
+    <div className='home-wrapper'>
+        <div className='title'>
+            <h1>Portfolio</h1>
+        </div>
+        <div className='version'>
+            <h1>2.0</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
